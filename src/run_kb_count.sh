816967d774311a67977eda73d83e64efd4914298
yak-liu-NEU <liu.yak@northeastern.edu>
2023-05-24 00:37:52 -0400

    exclude new sample out folders

--- src/run_kb_count.sh
+++ src/run_kb_count.sh
@@ -2,25 +2,12 @@
 
 ref_dir="$HOME/Code/10x_sequence_analysis_pipeline/ref"
 working_dir="$HOME/Code/10x_sequence_analysis_pipeline"
-samples="SRR7722938 SRR7722942"
-
-for sample in $samples; do
-
-    kb count \
+kb count \
     -i "$ref_dir/index_kb.idx" \
     -g "$ref_dir/t2g_kb.txt" \
     -x 10xv2 --h5ad \
-    --umi-gene \
-    --mm \
-    -o "$working_dir/count_out_$sample" \
+    -o "$working_dir/count_out" \
     -t 4 \
-    -m 4G \
-    --gene-names\
-    --report \
-    --overwrite \
-    --verbose \
-    "$working_dir/data/fastq/dumped/${sample}_2.fastq" \
-    "$working_dir/data/fastq/dumped/${sample}_3.fastq" \
+    "$working_dir/data/fastq/dumped/SRR7722942_2.fastq" \
+    "$working_dir/data/fastq/dumped/SRR7722942_3.fastq" \
     1>run_kb_count_std_out.txt 2>run_kb_count_std_err.txt
-
-done
